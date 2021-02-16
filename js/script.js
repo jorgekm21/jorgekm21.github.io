c71959169418d03551c0b5b4c8443bc2d048594f
Jorge Kassabji <jorgekm21@gmail.com>
2021-02-16 12:54:06 -0400

    Carga de libreria Typed.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,43 @@ const navbar = document.getElementById('navbar')
 const icon_navbar = document.getElementById('menu-btn')
 const menu = document.getElementById("menu")
 const elemento_i = document.getElementById("i")
+const typed = new Typed('.typed', {
+    strings: [
+        'Desarrollador', 
+        'Ingeniero Mecatronico', 
+        'Programador',
+    ],
+    typeSpeed: 30,
+    startDelay: 150,
+    backSpeed: 45,
+    smartBackspace: true,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+})
 
+const typed_two = new Typed('.typed_two', {
+    strings: [
+        'Programador',
+        'Desarrollador', 
+        'Ingeniero Mecatronico',
+        'Administrador BBDD',
+        'Implementador ERP', 
+    ],
+    typeSpeed: 30,
+    startDelay: 200,
+    backSpeed: 45,
+    smartBackspace: true,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+})
 addEventListener('scroll', ()=> {
     if(scrollY > 50){
         navbar.classList.add("navbar-two")
