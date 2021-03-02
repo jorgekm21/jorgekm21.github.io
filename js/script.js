241f705f3836d95256deaf159f2aa12575b3d67c
Jorge Kassabji <jorgekm21@gmail.com>
2021-03-02 10:37:35 -0400

    Preparativos de Lightbox

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@ const navbar = document.getElementById('navbar')
 const icon_navbar = document.getElementById('menu-btn')
 const menu = document.getElementById("menu")
 const elemento_i = document.getElementById("i")
+const cards = document.querySelectorAll(".card-project")
+
+addEventListener('scroll', ()=> {
+    if(scrollY > 50){
+        navbar.classList.add("navbar-two")
+    } else {
+        navbar.classList.remove("navbar-two")
+    }
+})
+
+
+cards.forEach(card => {
+    card.addEventListener('click', ()=>{
+        abrir_imagen(card.querySelector("img").getAttribute("src"))
+    })
+});
+
+// TODO Falta configurar la funcion para que abra el lightbox
+
+const abrir_imagen = (imagen) => {
+    console.log(imagen)
+}
+
+icon_navbar.addEventListener("click", ()=> {
+    menu.classList.toggle("active")
+    elemento_i.classList.toggle("active")
+})
+
+
 const typed = new Typed('.typed', {
     strings: [
         'Desarrollador', 
@@ -38,16 +67,4 @@ const typed_two = new Typed('.typed_two', {
     showCursor: true,
     cursorChar: '|',
     contentType: 'html',
-})
-addEventListener('scroll', ()=> {
-    if(scrollY > 50){
-        navbar.classList.add("navbar-two")
-    } else {
-        navbar.classList.remove("navbar-two")
-    }
-})
-
-icon_navbar.addEventListener("click", ()=> {
-    menu.classList.toggle("active")
-    elemento_i.classList.toggle("active")
 })
