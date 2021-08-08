db362aceddf765ef04ec1383a68b558c78937a64
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-08 10:27:00 -0400

    Typed Script

---
+++ src/public/js/script.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.text__work', {
+    strings: [
+        'Engineer', 
+        'Backend Dev', 
+        'FrontEnd Dev'],
+    typeSpeed: 30,
+    startDelay: 150,
+    backSpeed: 45,
+    smartBackspace: true,
+    backDelay: 1500,
+    loop: true,
+    loppCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+})
