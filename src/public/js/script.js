681d966d63edc5e00e3bda76e48cbd3949f4c105
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-16 10:26:28 -0400

    Formating files

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,16 +1,13 @@
-const typed = new Typed('.text__work', {
-    strings: [
-        'Engineer', 
-        'Backend Dev', 
-        'FrontEnd Dev'],
-    typeSpeed: 30,
-    startDelay: 150,
-    backSpeed: 45,
-    smartBackspace: true,
-    backDelay: 1500,
-    loop: true,
-    loppCount: false,
-    showCursor: true,
-    cursorChar: '|',
-    contentType: 'html'
-})
+const typed = new Typed(".text__work", {
+  strings: ["Engineer", "Backend Dev", "FrontEnd Dev"],
+  typeSpeed: 30,
+  startDelay: 150,
+  backSpeed: 45,
+  smartBackspace: true,
+  backDelay: 1500,
+  loop: true,
+  loppCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+});
